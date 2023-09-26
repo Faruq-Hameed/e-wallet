1747861dc899e01cee3d741b35200d376a47eeb3
Faruq-Hameed <faruqhameed1@gmail.com>
2023-09-26 21:58:21 +0100

    tested video length npm

--- app.js
+++ app.js
@@ -13,6 +13,15 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'))
 app.use(logger('common'))
 
+//get video length API
+const { getVideoDurationInSeconds } = require('get-video-duration')
+// From a URL...
+getVideoDurationInSeconds(
+    'http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4'
+  ).then((duration) => {
+    console.log({duration})
+  })
+
 //mongoose server configuration
 dbConnection()
 
