7d6a502435d1fe6f436f78a4927148c50d2fd488
Faruq-Hameed <faruqhameed1@gmail.com>
2023-10-18 16:56:53 +0100

    update app.js

--- app.js
+++ app.js
@@ -13,15 +13,6 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'))
 app.use(logger('common'))
 
-//get video length API
-const { getVideoDurationInSeconds } = require('get-video-duration')
-// From a URL...
-getVideoDurationInSeconds(
-    'http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4'
-  ).then((duration) => {
-    console.log({duration})
-  })
-
 //mongoose server configuration
 dbConnection()
 
